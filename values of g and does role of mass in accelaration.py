5795fcb494efb7255590df99f4e472e9f36adcee
prathvikgs <76068636+prathvikgs@users.noreply.github.com>
2022-07-08 23:31:42 +0530

    Update values of g and does role of mass in accelaration.py

--- values of g and does role of mass in accelaration.py
+++ values of g and does role of mass in accelaration.py
@@ -1,25 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[21]:
-
-
+'''
+given the data which consists the initial position,initial velocity,final position and the time taken to come to the final positiom,we can train a 
+model which gives the coefficients which relate to the final position, from this we can interpret many things,we get the value of accelaration
+due to gravity,we find that its motion dosen't really depend on it's mass as the coefficient corresponding to mass is 0,and we can get the equation
+of motion from it,it's just like rediscovering the equations of motion.
+'''
 import numpy as np
 import pandas as pd
 import csv 
 import math, random
 
 
-# In[22]:
-
-
 def location(x_0,v_0,t):
     x=x_0+v_0*t-(9.8/2)*t**2
     
     return x
-
-
-# In[32]:
 
 
 with open('location.csv',mode='w') as gravity_file:
@@ -33,70 +27,31 @@ with open('location.csv',mode='w') as gravity_file:
         gravity_writer.writerow([initial_position, initial_velocity, mass, time, location(initial_position, initial_velocity, time)])
 
 
-# In[33]:
-
-
+#Dataset
 dataset=pd.read_csv("location.csv")
 
 x=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,-1].values
 
 
-# In[34]:
-
-
-x.shape
-
-
-# In[35]:
-
-
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 
 regressor=LinearRegression()
+#we are gonna consider polynomials upto 2 degree.
 poly=PolynomialFeatures(degree=2)
 x=poly.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
 
-
-# In[36]:
-
-
-x.shape
-
-
-# In[37]:
-
-
 regressor.fit(x_train,y_train)
-
-
-# In[38]:
 
 
 coeff=regressor.coef_
 b=regressor.intercept_
 
-
-# In[39]:
-
-
 coeff=np.around(coeff,decimals=2)
 
-
-# In[40]:
-
-
-coeff
-
-
-# It can be seen that the coefficient corresponding to mass is 0,which indicates that the objects falling rate dosent depend on the mass of the object
-
-# In[ ]:
-
-
-
+print(coeff)
 
